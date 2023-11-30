5396f10603384ba6d7185737d9485613f8adf61f
Javier Gonzalez <developer.jgv@gmail.com>
2023-11-30 17:53:55 -0600

    Changes in adapter and item_superhero

--- app/src/main/java/com/aviva/marveltestapp/ui/adapters/SuperheroAdapter.kt
+++ app/src/main/java/com/aviva/marveltestapp/ui/adapters/SuperheroAdapter.kt
@@ -3,7 +3,6 @@ package com.aviva.marveltestapp.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -13,31 +12,37 @@ import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.aviva.marveltestapp.data.model.Character
 
-
 class SuperheroAdapter(
-    private var characterList: List<Character> = emptyList(),
+    private var characterPairs: List<Pair<Character, Character>> = emptyList(),
     private val onClick: (Character) -> Unit
 ) : RecyclerView.Adapter<SuperheroAdapter.ViewHolder>() {
 
     class ViewHolder(view: View, val onClick: (Character) -> Unit) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.imageViewSuperhero)
-        val textViewName: TextView = view.findViewById(R.id.textViewSuperheroName)
-        private var currentCharacter: Character? = null
+        val imageView1: ImageView = view.findViewById(R.id.imageViewSuperhero1)
+        val imageView2: ImageView = view.findViewById(R.id.imageViewSuperhero2)
+        val textViewName1: TextView = view.findViewById(R.id.textViewSuperheroName1)
+        val textViewName2: TextView = view.findViewById(R.id.textViewSuperheroName2)
+        private var currentCharacter1: Character? = null
+        private var currentCharacter2: Character? = null
 
         init {
             view.setOnClickListener {
-                currentCharacter?.let(onClick)
+                currentCharacter1?.let(onClick)
+                currentCharacter2?.let(onClick)
             }
         }
 
-        fun bind(character: Character) {
-            currentCharacter = character
-            textViewName.text = character.name
-            // Utiliza Picasso para cargar la imagen del personaje
+        fun bind(characterPair: Pair<Character, Character>) {
+            currentCharacter1 = characterPair.first
+            currentCharacter2 = characterPair.second
+            textViewName1.text = currentCharacter1?.name
+            textViewName2.text = currentCharacter2?.name
+
+            // Carga la imagen del primer personaje
             Picasso.get()
-                .load(character.thumbnail.getUrl())
+                .load(currentCharacter1?.thumbnail?.getUrl())
                 .placeholder(R.drawable.placeholder_image)
-                .into(imageView, object : Callback {
+                .into(imageView1, object : Callback {
                     override fun onSuccess() {
                         // La imagen se cargó correctamente
                     }
@@ -48,15 +53,34 @@ class SuperheroAdapter(
                         if (e is java.net.SocketTimeoutException) {
                             Toast.makeText(itemView.context, "Error 504: Timeout", Toast.LENGTH_SHORT).show()
                         }
-                        imageView.setImageResource(R.drawable.placeholder_image)
+                        imageView1.setImageResource(R.drawable.placeholder_image)
+                    }
+                })
+
+            // Carga la imagen del segundo personaje
+            Picasso.get()
+                .load(currentCharacter2?.thumbnail?.getUrl())
+                .placeholder(R.drawable.placeholder_image)
+                .into(imageView2, object : Callback {
+                    override fun onSuccess() {
+                        // La imagen se cargó correctamente
+                    }
+
+                    override fun onError(e: Exception?) {
+                        // Hubo un error al cargar la imagen
+                        e?.printStackTrace()
+                        if (e is java.net.SocketTimeoutException) {
+                            Toast.makeText(itemView.context, "Error 504: Timeout", Toast.LENGTH_SHORT).show()
+                        }
+                        imageView2.setImageResource(R.drawable.placeholder_image)
                     }
                 })
         }
     }
 
-    // Método para actualizar la lista de personajes
+    // Método para actualizar la lista de pares de personajes
     fun submitList(characters: List<Character>) {
-        characterList = characters
+        characterPairs = characters.chunked(2).map { Pair(it[0], it.getOrNull(1) ?: it[0]) }
         notifyDataSetChanged() // Notifica al adaptador que los datos han cambiado
     }
 
@@ -66,11 +90,9 @@ class SuperheroAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val character = characterList[position]
-        holder.bind(character)
-        val animation = AnimationUtils.loadAnimation(holder.itemView.context, R.anim.item_animation)
-        holder.itemView.startAnimation(animation)
+        val characterPair = characterPairs[position]
+        holder.bind(characterPair)
     }
 
-    override fun getItemCount() = characterList.size
+    override fun getItemCount() = characterPairs.size
 }
